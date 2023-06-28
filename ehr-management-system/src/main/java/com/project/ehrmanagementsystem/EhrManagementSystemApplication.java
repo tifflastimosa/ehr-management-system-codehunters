782119d311f6dfa5b1f9eab63d1940a73448befb
tifflastimosa <lastimosa.ti@northeastern.edu>
2023-06-27 23:47:32 -0700

    create spring boot project

---
+++ ehr-management-system/src/main/java/com/project/ehrmanagementsystem/EhrManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.ehrmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhrManagementSystemApplication.class, args);
+	}
+
+}
---
+++ ehr-management-system/src/test/java/com/project/ehrmanagementsystem/EhrManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ehrmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
