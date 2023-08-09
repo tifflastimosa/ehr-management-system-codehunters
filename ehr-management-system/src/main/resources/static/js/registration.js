e8cdab24940a0c12a867a5e4ce25661c07e5144e
tifflastimosa <lastimosa.ti@northeastern.edu>
2023-08-09 00:43:57 -0700

    remove old project

--- ehr-management-system/src/main/resources/static/js/registration.js
+++
@@ -1,8 +0,0 @@
-function confirmRegistration() {
-  var hasErrors = document.getElementsByClassName("errors").length > 0;
-  if (hasErrors) {
-    alert("There are errors in the form. Please correct them before submitting.");
-    return false; // Prevent form submission
-  }
-  return true; // Allow form submission
-}
